3db46eb93e83c2023c3175fbf3f756f00564ce39
Gabriel Gadelha <gabsgads@MacBook-Air-de-Gabriel.local>
2023-09-22 17:43:04 -0300

    projeto realizado no último dia do curso

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react'
+import Table from './Table'
+import Form from './Form'
+
+class App extends Component {
+  state={
+    alunos : [ ]
+  }
+  removerAluno = (index) => {
+    const {alunos} = this.state;
+    this.setState(
+        {
+          alunos: alunos.filter((aluno, i)=>{
+            return i != index;
+          })
+        }
+    );
+  }
+  handleSubmit = (alunoDisc) => {
+  
+      this.setState(
+        {
+          alunos: [...this.state.alunos, alunoDisc]
+        })
+  }
+  render(){
+    const {alunos} = this.state;
+    
+    return (
+      <div className="container">
+        <Table alunos = {alunos}
+          removerAluno = {this.removerAluno} />
+        <Form handleSubmit = {this.handleSubmit}/>  
+      </div>
+    );
+  }  
+} export default App;
